Fixed varval averaging issue

benchmarks/p3postproc/data_parse.py
@@ -34,9 +34,34 @@ def timestep(directory, type):
     # Generate the timestep array
     timestep_array = [[idx, ts_file[0], ts_file[1]] for idx, ts_file in enumerate(sorted_files)]
     
-    print(timestep_array[0][1])
+    #print(timestep_array[0][1])
 
     return timestep_array
+
+def get_var_lat_lon (data, variable_name, selected_time, selected_lev_ilev): #changeup this
+    """
+    Extract 2D array of variable value, latitude and longitude from the dataset based on the given variable name, timestamp, and lev value.
+    
+    Args:
+    - ds (xarray): The loaded dataset opened using xarray.
+    - variable_name (str): Name of the variable to extract.
+    - selected_time (str): Timestamp to filter the data.
+    - selected_lev_ilev (float): Level value to filter the data.
+    
+    Returns:
+    - var_lat_lon (array): 2D array of [variable values, lat, lon] for the given timestamp and lev.
+    """
+    
+    # Convert data to 2D array format: [variable values, lat, lon]
+    lat_values = data['lat'].values
+    lon_values = data['lon'].values
+    variable_values = data.values
+    
+    var_lat_lon = [[variable_values[i][j], lat_values[i], lon_values[j]] for i in range(len(lat_values)) for j in range(len(lon_values))]
+
+    return var_lat_lon
+
+
 
 def lat_lon_lev(dataset, variable_name, selected_time, selected_lev):
     """
@@ -61,23 +86,28 @@ def lat_lon_lev(dataset, variable_name, selected_time, selected_lev):
         raise ValueError("The variable "+variable_name+" doesn't use the dimensions 'lat', 'lon', 'lev'")
         return 0
 
-    selected_lev = valid_lev_ilev(ds, selected_lev, 'lev')  
 
-    # Extract the data for the given selected_time and lev
-    data = ds[variable_name].sel(time=selected_time, lev=selected_lev)
-    
     # Extract variable attributes
     variable_unit = ds[variable_name].attrs.get('units', 'N/A')
     variable_long_name = ds[variable_name].attrs.get('long_name', 'N/A')
     selected_ut = ds['ut'].sel(time=selected_time).values.item() / (1e9 * 3600)
     selected_mtime = get_mtime(ds,selected_time)
-
-    # Convert data to 2D array format: [variable values, lat, lon]
-    lat_values = data['lat'].values
-    lon_values = data['lon'].values
-    variable_values = data.values
     
-    var_lat_lon = [[variable_values[i][j], lat_values[i], lon_values[j]] for i in range(len(lat_values)) for j in range(len(lon_values))]
+
+    selected_levs = valid_lev_ilev(ds, selected_lev, 'lev')
+    #print(selected_levs)
+
+    # Extract the data for the given selected_time and lev
+    
+    if len(selected_levs) == 1:
+        data = ds[variable_name].sel(time=selected_time, lev=selected_levs[0])
+        var_lat_lon = get_var_lat_lon(data, variable_name, selected_time, selected_levs[0])
+    elif len(selected_levs) == 2:
+        data1 = ds[variable_name].sel(time=selected_time, lev=selected_levs[0])
+        var_lat_lon1 = get_var_lat_lon(data1, variable_name, selected_time, selected_levs[0])
+        data2 = ds[variable_name].sel(time=selected_time, lev=selected_levs[1])
+        var_lat_lon2 = get_var_lat_lon(data2, variable_name, selected_time, selected_levs[1])
+        var_lat_lon = avg_var_lat_lon(var_lat_lon1, var_lat_lon2)
     
 
     return var_lat_lon, selected_lev, variable_unit, variable_long_name, selected_ut, selected_mtime
@@ -106,69 +136,91 @@ def lat_lon_ilev(dataset, variable_name, selected_time, selected_ilev):
         raise ValueError("The variable "+variable_name+" doesn't use the dimensions 'lat', 'lon', 'ilev'")
         return 0
  
-    selected_ilev = valid_lev_ilev(ds, selected_ilev, 'ilev')
-
-    # Extract the data for the given selected_time and ilev
-    data = ds[variable_name].sel(time=selected_time, ilev=selected_ilev)
     
+
     # Extract variable attributes
     variable_unit = ds[variable_name].attrs.get('units', 'N/A')
     variable_long_name = ds[variable_name].attrs.get('long_name', 'N/A')
     selected_ut = ds['ut'].sel(time=selected_time).values.item() / (1e9 * 3600)
     selected_mtime=get_mtime(ds,selected_time)
 
-    # Convert data to 2D array format: [variable values, lat, lon]
-    lat_values = data['lat'].values
-    lon_values = data['lon'].values
-    variable_values = data.values
+    selected_ilevs = valid_lev_ilev(ds, selected_ilev, 'ilev')
     
-    var_lat_lon = [[variable_values[i][j], lat_values[i], lon_values[j]] for i in range(len(lat_values)) for j in range(len(lon_values))]
+    # Extract the data for the given selected_time and ilev
+    
+
+    if len(selected_ilevs) == 1:
+        data = ds[variable_name].sel(time=selected_time, ilev=selected_ilevs[0])
+        var_lat_lon = get_var_lat_lon(data, variable_name, selected_time, selected_ilevs[0])
+    elif len(selected_ilevs) == 2:
+        data1 = ds[variable_name].sel(time=selected_time, ilev=selected_ilevs[0])
+        var_lat_lon1 = get_var_lat_lon(data1, variable_name, selected_time, selected_ilevs[0])
+        data2 = ds[variable_name].sel(time=selected_time, ilev=selected_ilevs[1])
+        var_lat_lon2 = get_var_lat_lon(data2, variable_name, selected_time, selected_ilevs[1])
+        var_lat_lon = avg_var_lat_lon(var_lat_lon1, var_lat_lon2)
+
     
     return var_lat_lon, selected_ilev, variable_unit, variable_long_name, selected_ut, selected_mtime
 
 
 
 def valid_lev_ilev(ds, selected_lev_ilev, lev_ilev):
-    """Checks if the given lev or ilev value is in the dataset and if not gets the closest valid value
+    """
+    Checks if the given lev or ilev value is in the dataset and if not gets the two closest valid values.
     
     Parameters:
     - ds (xarray): The loaded dataset opened using xarray.
-    - selected_time (str): Timestamp to filter the data.
+    - selected_lev_ilev (float): Desired lev or ilev value.
+    - lev_ilev (str): Dimension name ('lev' or 'ilev') in the dataset.
     
     Returns:
-    - selected_lev_ilev (float): A value of lev or ilev present in the dataset.
+    - selected_levs (list): A list containing one or two values of lev or ilev present in the dataset.
     """
-    valid_ilev= ds[lev_ilev].values
+    valid_levs_ilevs = ds[lev_ilev].values
 
-    if selected_lev_ilev not in valid_ilev:
-        print("The "+lev_ilev+" "+ str(selected_lev_ilev)+" isn't in the listed valid values")
-        valid_ilev = np.asarray(valid_ilev)
-        idx = (np.abs(valid_ilev - selected_lev_ilev)).argmin()
-        selected_lev_ilev = valid_ilev[idx]
-        print("Selecting the closest valid "+lev_ilev+" "+ str(selected_lev_ilev))  
-    return selected_lev_ilev
+    if selected_lev_ilev not in valid_levs_ilevs:
+        print(f"The {lev_ilev} {selected_lev_ilev} isn't in the listed valid values.")
+        valid_levs_ilevs = np.asarray(valid_levs_ilevs)
+        idx = (np.abs(valid_levs_ilevs - selected_lev_ilev)).argsort()[:2]
+        selected_levs_ilevs = valid_levs_ilevs[idx]
+        print(f"Averaging from the closest valid {lev_ilev}s: {selected_levs_ilevs[0]} and {selected_levs_ilevs[1]}")
+    else:
+        selected_levs_ilevs = [selected_lev_ilev]
+
+    return selected_levs_ilevs
+
 
 def calc_avg_ht(data_array, time, lev, dataset):
     """
-    Compute the average ZG value for a given set of lat, lon, and lev from a dataset.
+    Compute the average Z value for a given set of lat, lon, and lev from a dataset.
     
     Parameters:
     - data_array (list): A list of [varval, lat, lon].
     - lev (float): The level for which to retrieve data.
-    - dataset: The dataset containing the ZG variable.
+    - dataset: The dataset containing the Z variable.
     
     Returns:
-    - float: The average ZG value for the given conditions.
+    - float: The average Z value for the given conditions.
     """
     total = 0
     count = len(data_array)
     ds = xr.open_dataset(dataset)
-    lev = valid_lev_ilev(ds, lev, 'ilev')  
-    for entry in data_array:
-        lat, lon = entry[1], entry[2]
-        total += ds['ZG'].sel(time=time, lat=lat, lon=lon, ilev=lev).item()
-        
-    return round((total / count if count != 0 else 0)/100000,2)
+    levs = valid_lev_ilev(ds, lev, 'ilev')
+    if len(levs) == 1:
+        for entry in data_array:
+            lat, lon = entry[1], entry[2]
+            total += ds['Z'].sel(time=time, lat=lat, lon=lon, ilev=levs[0]).item()
+        return round((total / count if count != 0 else 0)/100000,2)
+    if len(levs) == 2:
+        for entry in data_array:
+            lat, lon = entry[1], entry[2]
+            total += ds['Z'].sel(time=time, lat=lat, lon=lon, ilev=levs[0]).item()
+        avg_ht_1=(total / count if count != 0 else 0)/100000
+        for entry in data_array:
+            lat, lon = entry[1], entry[2]
+            total += ds['Z'].sel(time=time, lat=lat, lon=lon, ilev=levs[1]).item()
+        avg_ht_2=(total / count if count != 0 else 0)/100000
+        return round((avg_ht_1+avg_ht_2)/2,2)
 
 
 def min_max(data_array):
@@ -212,3 +264,37 @@ def get_mtime(ds, selected_time):
         return mtime_variable[index[0][0]]
     else:
         return None
+
+
+def avg_var_lat_lon(var_lat_lon1, var_lat_lon2):
+    """
+    Computes the average variable value for each lat-lon pair from two var_lat_lon arrays.
+    
+    Args:
+    - var_lat_lon1 (array): First list of [variable values, lat, lon].
+    - var_lat_lon2 (array): Second list of [variable values, lat, lon].
+    
+    Returns:
+    - avg_var_lat_lon (array): List of averaged [variable values, lat, lon] for each lat-lon pair.
+    """
+    
+    # Create a dictionary with lat-lon pairs as keys and their values as lists
+    lat_lon_dict = {}
+    
+    for var_val, lat, lon in var_lat_lon1:
+        if (lat, lon) in lat_lon_dict:
+            lat_lon_dict[(lat, lon)].append(var_val)
+        else:
+            lat_lon_dict[(lat, lon)] = [var_val]
+    
+    for var_val, lat, lon in var_lat_lon2:
+        if (lat, lon) in lat_lon_dict:
+            lat_lon_dict[(lat, lon)].append(var_val)
+        else:
+            lat_lon_dict[(lat, lon)] = [var_val]
+            
+    # Compute the average for each lat-lon pair
+    avg_var_lat_lon = [[sum(lat_lon_dict[(lat, lon)]) / len(lat_lon_dict[(lat, lon)]), lat, lon] 
+                       for lat, lon in lat_lon_dict]
+    
+    return avg_var_lat_lon