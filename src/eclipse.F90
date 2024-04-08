NCAR/tiegcm

commit fa8c6cb6c65863b5717b4bf278ad520bbd6bd331

Refactor eclipse module
The eclipse related codes are refactored to better represent the logic. The constants in the original module are moved outside of the code as an additional input data (eclipse_list).

src/calculate_ecf.F
@@ -1,131 +1,52 @@
-!
-      module calculate_ecf_module
-!
-! Coded by Tong Dang, August, 2017
-! This program is to calculate the rest euv flux during eclipse period
-      use params_module,only: nlon,nlonp4,nlat
-      use eclipse_module, only: eclip
-!
+      subroutine calculate_ecf(mag_factor,eclipse_factor,
+     |  lon0,lon1,lev0,lev1,lat)
+! calculate the rest EUV flux during eclipse period, Dang 2017
+
+      use params_module,only: nlon,nlonp4,glat,glon
+      use cons_module,only: re,pi
+      use hist_module,only: modeltime
+      use chapman_module,only: chi
+      use init_module,only: iyear,iday
+      use eclipse_module,only: eclipse
+
       implicit none
-!
-      contains
-!-----------------------------------------------------------------------
-      subroutine calculate_ecf(eclipfactor,lon0,lon1,lev0,lev1,lat
-     | ,newmag_factor)
-!
-! coded by Dang, 2017
-      use params_module,only: glon,glat
-      use hist_module, only: modeltime
-      use chapman_module, only: chi
-      use init_module, only: iyear, iday
-! Args:
-      integer,intent(in) :: lat,lon0,lon1,lev0,lev1
-      real,dimension(lev0:lev1,lon0:lon1),intent(out) :: eclipfactor
-     | ,newmag_factor
-! Local:
-      real :: longitude,latitude,height,ut
-      real*8 :: newmag
-      real :: t1_SLT,t4_SLT,tm_SLT,SLT,mag,EclipseFactor
-      integer :: k,i
+
+      integer,intent(in) :: lon0,lon1,lev0,lev1,lat
+      real,dimension(lev0:lev1,lon0:lon1),intent(out) ::
+     |  mag_factor,eclipse_factor
+
+      integer :: month,day,i,ishift
+      real :: height,rekm,sza,longitude,latitude,ut,slt,mag,fac
 
 ! Currently height is fixed at 300km for all altitudes
-      height=300.
-      ut=modeltime(2)+modeltime(3)/60.+modeltime(4)/3600.
+      height = 300
 
-!      if(modeltime(1) .gt. 233 .or. ut .gt. 21.2 .or. ut .lt. 15.6 .or.
-!     . modeltime(1) .lt. 233) then
-!         eclipfactor(lev0:lev1,lon0:lon1)=1.0
-!         newmag_factor(lev0:lev1,lon0:lon1)=0.0
-!      else
+      rekm = re*1e-5
+      sza = pi-asin(rekm/(rekm+height))
 
-      do i=lon0,lon1
+      call to_month_day(iyear,iday,month,day)
+      ut = modeltime(2)+modeltime(3)/60+modeltime(4)/3600
 
-          if(i .ne. 1 .and. i .ne. 2 .and. i .ne. nlon+3 .and. 
-     . i .ne. nlon+4) then
-           longitude=glon(i-2)
-           latitude=glat(lat)
-           SLT=modeltime(2)+modeltime(3)/60.+modeltime(4)/3600.
-     . + longitude/15.
-           if(SLT .LT. 0) then
-              SLT = SLT + 24.
-           endif
-           if(SLT .GT. 24) then
-              SLT = SLT - 24.
-           endif
-         
-          if(chi(i,lat)/3.14159*180. .le. (180.-asind(6371.0/(
-     .6371.0+height)))) then 
-           call eclip(ut,SLT,longitude,latitude,height,newmag, 
-     .           t1_SLT,t4_SLT,tm_SLT,mag, iyear, iday)
-           call Eclipse(newmag,EclipseFactor)
-           eclipfactor(lev0:lev1,i)=EclipseFactor
-           newmag_factor(lev0:lev1,i)=newmag
-           else
-             eclipfactor(lev0:lev1,i)=1.0
-             newmag_factor(lev0:lev1,i)=0.0
-           endif
-
-          endif
-       enddo
-   
-      if(lon0 .eq. 1) then 
-        eclipfactor(lev0:lev1,1)=eclipfactor(lev0:lev1,3)
-        eclipfactor(lev0:lev1,2)=eclipfactor(lev0:lev1,3)
-      endif
-      if(lon1 .eq. nlon+4)then
-        eclipfactor(lev0:lev1,nlon+3)=eclipfactor(lev0:lev1,nlon+2)
-        eclipfactor(lev0:lev1,nlon+4)=eclipfactor(lev0:lev1,nlon+2)
-      endif
-
-!      endif
-      end subroutine calculate_ecf
-!-----------------------------------------------------------------------
-	Subroutine Eclipse(ecf,EclipseFactor)
-CC    calculation of the ratio of φTi / φT0 = (12.3Spi+Sci)/(12.3sp0 + Sc0)
-CC    calculation of the unmasked area for photosphere for P and C regions
-CC    during the solar eclipse.
-CC    P region is an inner central disk over the "photosphere";
-CC    C region is an outer ring between the "coronal" and "phosphere".
-CC    reference to J. J Curto et al.(2006), Modeling the geomagnetic effects 
-CC    caused by the solar eclipse of 11 August 1999.
-
-CC    initialize 
-        implicit real*8 (A-Z)
-        Rp=1.0; Rc=2.1; 
-	  pi=3.1415926;
-	  Sp0=pi*Rp*Rp;
-	  Sc0=pi*(Rc*Rc-Rp*Rp);
-CC      dl should be smaller than Rp+Rc; that is dl <= 3.1
-C         v=3.1*2/Duration !! for duration=240 mins v= 3.1/120 mins =0.025833
-C 	                   !! for duration=180 mins v= 3.1/90 mins =0.034444
-C         dl=dabs(3.1-v*eclipseTime);  
-        dl=2.0-ecf*2;
-
-        if(dl<=Rc-Rp) then 
-	      A1=0;
-            E=2*Rp*Rp*acos(dl/2/Rp) - dl*sqrt(Rp*Rp - dl*dl*0.25);
-	  end if
-	  if(dl>Rc-Rp .AND. dl<2*Rp) then
-	      E=2*Rp*Rp*acos(dl/2/Rp) - dl*sqrt(Rp*Rp - dl*dl*0.25);
-	      Xd=(Rc*Rc - Rp*Rp + dl*dl)/2/dl;
-            A2=Rc*Rc*acos(Xd/Rc) - Xd*sqrt(Rc*Rc -Xd*Xd);
-	      A3=Rp*Rp*acos((dl-Xd)/Rp) - (dl-Xd)*
-     & 	     sqrt(Rp*Rp-(dl-Xd)*(dl-Xd));
-            A1=pi*Rp*Rp - A2 -A3;
-	  endif
-	  if(dl>=2*Rp .AND. dl<=3.1) then
-	      E=0;
-	      Xd=(Rc*Rc - Rp*Rp + dl*dl)/2/dl;
-            A2=Rc*Rc*acos(Xd/Rc) - Xd*sqrt(Rc*Rc -Xd*Xd);
-	      A3=Rp*Rp*acos((dl-Xd)/Rp) - (dl-Xd)*
-     & 	     sqrt(Rp*Rp-(dl-Xd)*(dl-Xd));
-            A1=pi*Rp*Rp - A2 -A3;
+      latitude = glat(lat)
+      do i = lon0,lon1
+        if (i==1 .or. i==2) then
+          ishift = i-2+nlon
+        elseif (i==nlonp4-1 .or. i==nlonp4) then
+          ishift = i-2-nlon
+        else
+          ishift = i-2
         endif
+        longitude = glon(ishift)
+        slt = modulo(ut+longitude/15, 24.0)
+        if (chi(i,lat) <= sza) then
+          call eclipse(iyear,month,day,
+     |      latitude,longitude,height,ut,slt,mag,fac)
+          mag_factor(lev0:lev1,i) = mag
+          eclipse_factor(lev0:lev1,i) = fac
+        else
+          mag_factor(lev0:lev1,i) = 0
+          eclipse_factor(lev0:lev1,i) = 1
+        endif
+      enddo
 
-	  Sc=pi*(Rc*Rc - 2*Rp*Rp) + E + A1;
-        Sp=pi*Rp*Rp - E;
-	  EclipseFactor=(12.3*Sp+Sc)/(12.3*Sp0+Sc0);
-	  if(ecf<0.0001) EclipseFactor=1.0
-	End Subroutine Eclipse
-!-----------------------------------------------------------------------
-      end module calculate_ecf_module
+      end subroutine calculate_ecf

src/qrj.F
@@ -78,7 +78,6 @@
       use lbc,only: fb,b
       use chemrates_module,only: beta9
       use fields_module,only: tlbc
-      use calculate_ecf_module,only: calculate_ecf
 !
 ! Args:
       integer,intent(in) :: lev0,lev1,lon0,lon1,lat
@@ -115,9 +114,7 @@
      |  quenchfac, ! (s8)
      |  sigchap,   ! (s9)
      |  p3f        ! (s7)
-      real,dimension(lev0:lev1,lon0:lon1) ::
-     |  eclipfactor,
-     |  newmag_factor
+      real,dimension(lev0:lev1,lon0:lon1) :: mag_factor,eclipse_factor
 
 ! temporary loop variables 
       real::
@@ -161,17 +158,15 @@
 !     call addfsech('XNMBARI',' ',' ',xnmbari(:,lon0:lon1),
 !    |  lon0,lon1,nlevs,nlevs,lat)
 !
-
       if (doEclipse) then
-        call calculate_ecf(eclipfactor,lon0,lon1,lev0,lev1,lat,
-     |    newmag_factor)
-        call addfld('EUV_factor' ,' ',' ', eclipfactor(lev0:lev1,
-     |  lon0:lon1),
-     |  'lev',lev0,lev1,'lon',lon0,lon1,lat)
-        call addfld('newmag' ,' ',' ', newmag_factor(lev0:lev1,
-     |  lon0:lon1),
-     |  'lev',lev0,lev1,'lon',lon0,lon1,lat)
+        call calculate_ecf(mag_factor,eclipse_factor,
+     |    lev0,lev1,lon0,lon1,lat)
+        call addfld('mag_factor',' ',' ',mag_factor,
+     |    'lev',lev0,lev1,'lon',lon0,lon1,lat)
+        call addfld('eclipse_factor',' ',' ',eclipse_factor,
+     |    'lev',lev0,lev1,'lon',lon0,lon1,lat)
        endif
+
 !  calculate inverse of wave length
 !
       do i=1,lmax
@@ -255,18 +250,11 @@
      |                            sigeuv(3,l)*scn2(k,i)
 !
             if (.not.check_exp) then
-              if (doEclipse) then
-              sum1(k,i) = feuv(l)*exp(-sum1(k,i))*eclipfactor(k,i)
-              else
               sum1(k,i) = feuv(l)*exp(-sum1(k,i))
-              endif
             else
-              if (doEclipse) then
-              sum1(k,i) = feuv(l)*expo(-sum1(k,i),0)*eclipfactor(k,i)
-              else
               sum1(k,i) = feuv(l)*expo(-sum1(k,i),0)
-              endif
             endif
+            if (doEclipse) sum1(k,i) = sum1(k,i)*eclipse_factor(k,i)
 
             sum2(k,i) = sum2(k,i)+sigeuv(1,l)*mn_o2(k,i)+
      |                            sigeuv(2,l)*mn_o1(k,i)+
@@ -400,9 +388,8 @@
               sigchap(k,i) = sigsrc(l)*fsrc(l)*
      |          expo(-sigsrc(l)*sco2(k,i),0)
             endif
-            if(doEclipse) then
-              sigchap(k,i) = sigchap(k,i) * eclipfactor(k,i)
-            endif
+            if (doEclipse)
+     |        sigchap(k,i) = sigchap(k,i)*eclipse_factor(k,i)
 !
             sum1(k,i) = sum1(k,i)+sigchap(k,i)*
      |        (hc*rlmsrcinv(l)-do22+quenchfac(k,i))

src/advance.F
@@ -22,7 +22,8 @@
       use wei05sc,only: weimer05        ! Weimer 2005 model
       use input_module,only: pristart,step,idynamo=>dynamo,
      |  calendar_advance,gpi_ncfile,potential_model,wei05sc_ncfile,
-     |  imf_ncfile,current_pg,current_kq,iamie,amie_ibkg
+     |  imf_ncfile,current_pg,current_kq,iamie,amie_ibkg,
+     |  doEclipse,eclipse_list
       use input_module,only: 
      |  power  ,power_time  ,ntimes_power, rd_power,
      |  ctpoten,ctpoten_time,ntimes_ctpoten, rd_ctpoten,
@@ -53,6 +54,7 @@
       use output,only: outhist,output_hist
       use addfld_module,only: addfld
       use lbc,only: tuvz_lbc
+      use eclipse_module,only: read_eclipse_list,find_eclipse
 !
 ! Routines and timing for parallel dynamo (pdynamo.F):
       use pdynamo_module,only: dynamo_inputs,pdynamo,pefield,
@@ -76,7 +78,7 @@
 #endif
 !
 ! Local:
-      integer :: i,j,itmp,ier,iprint
+      integer :: i,j,itmp,ier,iprint,month,day
       integer :: cmodeltime(4) !MAGE coupling time
       real :: fmin,fmax
       integer(kind=8) :: nsecs ! current model time in seconds,
@@ -160,6 +162,9 @@
       itmp = itp
       itp = itc
       itc = itmp
+!
+! Initialize eclipse list with external data
+      call read_eclipse_list(eclipse_list)
 !
       call timer(time0_init,time1_init,'INIT',1,0) ! end init timing
       if (timing%level >= 2)
@@ -273,6 +278,12 @@
 ! Update lower boundaries of t,u,v,z:
 !
       call tuvz_lbc(istep,iday,secs)
+!
+! Update eclipse elements array if necessary:
+      if (doEclipse) then
+        call to_month_day(iyear,iday,month,day)
+        call find_eclipse(iyear,month,day)
+      endif
 !
 ! Receive aurora parameters from the M-I coupler for MAGE
 !

src/input.F
@@ -31,9 +31,11 @@
 !       to the input values. These may be referenced from included
 !       header files or other modules).
 !
+      integer,parameter :: mxlen_filename=1024
+!
 ! Namelist user input variables:
 !
-      character(len=1024) :: 
+      character(len=mxlen_filename) :: 
      |  label,           ! optional generic text label for this run
      |  amievol,         ! absolute or relative path to amie data file (optional)
      |  amiesh,          ! file or mss path of amie SH data file (optional)
@@ -90,7 +92,9 @@
      |  ntimes_f107,ntimes_f107a
       logical :: aluse,  ! logical to use AL in Weimer 2001 model or not
      |  et,              ! logical to calculate electron turbulent heating
-     |  saps             ! logical to include empirical SAPS
+     |  saps,            ! logical to include empirical SAPS
+     |  doEclipse,       ! logical to do eclipse mask or not
+     |  oneway           ! logical to use oneway mix reader
 !
 ! Parameters as read from namelist:
       real :: rd_power,rd_ctpoten,rd_f107,rd_f107a,rd_bximf,rd_byimf,
@@ -103,7 +107,6 @@
       integer :: indices_interp=1 
 !
 ! Import data file names:
-      integer,parameter :: mxlen_filename=1024
       character(len=mxlen_filename) ::
 !
 ! 4/2/08 btf: Introducing Weimer 2005 model (wei05sc.F).
@@ -133,11 +136,9 @@
      |  he_coefs_ncfile,    ! coefficients of Helium flux at upper boundary
      |  vedata,             ! SAPS ve data file
      |  vndata,             ! SAPS vn data file
-     |  vvdata              ! SAPS vv data file
-
-      logical :: oneway   ! logical to use oneway mix reader
-      logical :: doEclipse ! Do eclipse mask or not
-      character(len=mxlen_filename) :: mixfile ! filename for oneway driving
+     |  vvdata,             ! SAPS vv data file
+     |  eclipse_list,       ! Eclipse list data file
+     |  mixfile             ! filename for oneway driving
 
 !     integer,parameter :: ngpivars = 4
 !     real :: gpi_vars(ngpivars) ! f107,f107a,power,ctpoten
@@ -186,7 +187,7 @@
      |  f107a_time,hpss_path,current_pg,current_kq,calc_helium,
      |  bgrddata_ncfile,ctmt_ncfile,opdiffcap,electron_heating,
      |  amienh,amiesh,amie_ibkg,he_coefs_ncfile,et,saps,
-     |  vedata,vndata,vvdata,oneway,doEclipse,mixfile
+     |  vedata,vndata,vvdata,doEclipse,eclipse_list,oneway,mixfile
 !
 ! List of fields that are always written to secondary histories:
       character(len=16) :: secflds_mandatory(6) =
@@ -271,6 +272,8 @@
       vedata = ' '
       vndata = ' '
       vvdata = ' '
+      eclipse_list = ' '
+      mixfile = 'msphere.mix.h5'
 !
       start_day  = ispval
       start_year = ispval
@@ -297,10 +300,8 @@
       electron_heating = ispval
       et = .false.
       saps = .false.
-
-      oneway = .false.
       doEclipse = .false.
-      mixfile = 'msphere.mix.h5'
+      oneway = .false.
 
       f107    = spval
       f107a   = spval
@@ -634,6 +635,11 @@
       if (len_trim(vvdata) == 0) vvdata = '$TGCMDATA/vv.dat'
       call expand_path(vvdata)
 !
+! Eclipse list data file:
+      if (len_trim(eclipse_list) == 0)
+     |  eclipse_list = '$TGCMDATA/eclipse_list.nc'
+      call expand_path(eclipse_list)
+!
 ! 1/12/05 btf: removed old dynamo (was in dynamo_old.F)
 ! 6/11/08 btf: remove dynamo==0 option
 !
@@ -765,8 +771,8 @@
       integer(kind=8) :: step8,
      |  nsec_start(mxseries),nsec_stop(mxseries),
      |  nsec_hist (mxseries)
-      character(len=1024) :: ch1024
-      character(len=1024) :: hvols(mxhvols)
+      character(len=mxlen_filename) :: ch1024
+      character(len=mxlen_filename) :: hvols(mxhvols)
 !
 ! External:
       integer,external :: numfiles
@@ -1080,8 +1086,8 @@
       integer(kind=8) :: step8,
      |  nsec_start(mxseries_sech),nsec_stop(mxseries_sech),
      |  nsec_hist (mxseries_sech)
-      character(len=1024) :: ch1024
-      character(len=1024) :: hvols(mxhvols)
+      character(len=mxlen_filename) :: ch1024
+      character(len=mxlen_filename) :: hvols(mxhvols)
 !
 ! External:
       integer,external :: numfiles

src/util.F
@@ -1381,3 +1381,27 @@
       return
       end
 !-------------------------------------------------------------------
+      subroutine to_month_day(year,doy,month,day)
+      implicit none
+!
+! convert doy to month,day pair
+!
+      integer,intent(in) :: year,doy
+      integer,intent(out) :: month,day
+
+      integer,dimension(12) :: days_in_month
+
+      days_in_month = (/31,28,31,30,31,30,31,31,30,31,30,31/)
+      if (mod(year,400)==0 .or.
+     |  (mod(year,4)==0 .and. mod(year,100)/=0)) days_in_month(2) = 29
+
+      day = doy
+      do month = 1,12
+        if (day > days_in_month(month)) then
+          day = day - days_in_month(month)
+        else
+          exit
+        endif
+      enddo
+
+      end subroutine to_month_day

commit 11113e91d46323778948636f8e4259010748f5ed

Bugfix for eclipse calculation
When calculating UT in calculate_ecf, decimal points are erroneously truncated leading to the incorrect UT. This corrects the error introduced in commit fa8c6cb.

src/calculate_ecf.F
@@ -1,8 +1,8 @@
       subroutine calculate_ecf(mag_factor,eclipse_factor,
      |  lon0,lon1,lev0,lev1,lat)
-! calculate the rest EUV flux during eclipse period, Dang 2017
+! calculate the rest EUV flux during eclipse period, Dang 2017/08
 
-      use params_module,only: nlon,nlonp4,glat,glon
+      use params_module,only: nlon,glat,glon
       use cons_module,only: re,pi
       use hist_module,only: modeltime
       use chapman_module,only: chi
@@ -25,17 +25,13 @@
       sza = pi-asin(rekm/(rekm+height))
 
       call to_month_day(iyear,iday,month,day)
-      ut = modeltime(2)+modeltime(3)/60+modeltime(4)/3600
+      ut = modeltime(2)+modeltime(3)/60.0+modeltime(4)/3600.0
 
       latitude = glat(lat)
       do i = lon0,lon1
-        if (i==1 .or. i==2) then
-          ishift = i-2+nlon
-        elseif (i==nlonp4-1 .or. i==nlonp4) then
-          ishift = i-2-nlon
-        else
-          ishift = i-2
-        endif
+        ishift = i-2
+        if (ishift <= 0) ishift = ishift+nlon
+        if (ishift >= nlon+1) ishift = ishift-nlon
         longitude = glon(ishift)
         slt = modulo(ut+longitude/15, 24.0)
         if (chi(i,lat) <= sza) then

src/eclipse.F90
@@ -257,7 +257,7 @@ module eclipse_module
     observer_constants(2) = height*1e3
     observer_constants(3) = 0
 
-    latr = observer_constants(0)*dtr
+    latr = latitude*dtr
     reduced_lat = atan(aspect_ratio*tan(latr))
     observer_constants(4) = aspect_ratio*sin(reduced_lat) + observer_constants(2)/re*sin(latr)
     observer_constants(5) =              cos(reduced_lat) + observer_constants(2)/re*cos(latr)
@@ -424,7 +424,7 @@ module eclipse_module
 
 ! Entry conditions -
 !   1. The mid array must be populated
-!   2. There mut be either a total or annular eclipse at the location!
+!   2. There must be either a total or annular eclipse at the location!
 
     real :: tmp,n
   

src/qrj.F
@@ -166,7 +166,7 @@
         call addfld('eclipse_factor',' ',' ',eclipse_factor,
      |    'lev',lev0,lev1,'lon',lon0,lon1,lat)
        endif
-
+!
 !  calculate inverse of wave length
 !
       do i=1,lmax