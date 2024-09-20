f5a6ab2545631719ec1efa5675c17991939666ca
cookieenick <nikhilr@ucar.edu>
2024-09-20 09:24:46 -0600

    Coupling fix + code refactor

---
+++ tiegcmrun/engage_solver.py
@@ -0,0 +1,188 @@
+"""
+engage_solver.py for the TIEGCMrun software.
+
+This script helps in solving for the correct input and pbs parameters for running the TIEGCM model in coupled mode with Engage. 
+
+Functions included:
+
+- gamres_to_res(gamres): Converts the GAMERA grid type to horizontal resolution values.
+- engage_parser(engage_parameters): Parses the engage.json file and returns the options dictionary.
+- get_engage_start_time(datetime_str, seconds): Calculates the start time for the Engage run by subtracting the spin-up time from the coupled start date.
+- engage_run(options, debug, coupling, engage): Prepares and runs the TIEGCM model in both standalone and coupled modes, generating the necessary input and PBS files.
+"""
+
+import os
+import json
+import copy
+from datetime import datetime, timedelta
+
+from misc import seconds_to_dhms, resolution_solver, find_file, select_resource_defaults
+from output_solver import segment_inp_pbs
+from interpolation import interpic
+
+
+# Path to current tiegcm datafiles
+TIEGCMDATA = os.environ["TIEGCMDATA"]
+# Path to current tiegcm installation
+TIEGCMHOME = os.environ["TIEGCMHOME"]
+# Path to directory containing support files for makeitso.
+SUPPORT_FILES_DIRECTORY = os.path.join(TIEGCMHOME, "tiegcmrun")
+OPTION_DESCRIPTIONS_FILE = os.path.join(SUPPORT_FILES_DIRECTORY, "options_description.json")
+
+def gamres_to_res(gamres):
+    "D", "Q", "O", "H"
+    if gamres == "D":
+        return 2.5 , 2.5
+    elif gamres == "Q":
+        return 2.5 , 1.25
+    elif gamres == "O":
+        return 1.25 , 0.625
+    elif gamres == "H":
+        return 1.25 ,0.625
+
+def engage_parser(engage_parameters):
+    """
+    Parse the engage.json file and return the options dictionary.
+
+    Args:
+        engage_jsonfile (str): The path to the engage.json file.
+
+    Returns:
+        dict: The options dictionary.
+    """
+
+    o = engage_parameters["simulation"]
+    
+    hpc_system = o['hpc_system']
+    coupled_job_name = o['job_name']
+    coupled_start_date = o['start_date']
+    stop_date = o['stop_date']
+
+    use_segments = o['use_segments']
+    segment_duration = int(float((o['segment_duration'])))
+    segment = seconds_to_dhms(segment_duration)
+    horires_standalone, horires_coupled = gamres_to_res(o['gamera_grid_type'])
+    
+    o = engage_parameters["pbs"]
+    account_name = o['account_name']
+    queue = o['queue']
+    job_priority = o['job_priority']
+    walltime = o['walltime']
+    modules = o['modules']
+
+    o = engage_parameters["coupling"]
+    
+    gamera_spin_up_time = int(o['gamera_spin_up_time'])
+    gcm_spin_up_time = int(o['gcm_spin_up_time'])
+    conda_env = o['conda_env']
+    
+    start_date = get_engage_start_time(coupled_start_date,gamera_spin_up_time+gcm_spin_up_time)
+    
+    o = engage_parameters["voltron"]
+    voltron_dtOut = int(float(o['output']['dtOut']))
+    hist = seconds_to_dhms(voltron_dtOut)
+    STEP = int(float(o['coupling']['dtCouple']))
+
+    root_directory= os.path.abspath(os.curdir)
+    eo = engage_options = {}
+    
+    eo['job_name'] = coupled_job_name
+    eo['hpc_system'] = hpc_system
+    eo['start_time'] = start_date
+    eo['coupled_start_time'] = coupled_start_date
+    eo['stop_time'] = stop_date
+    eo['segment'] = segment
+    eo['segment_seconds'] = segment_duration
+    eo['horires'] = horires_standalone
+    eo['horires_coupled'] = horires_coupled
+    eo['STEP'] = STEP
+    eo["voltron_dtOut"] = voltron_dtOut
+    eo['parentdir'] = root_directory
+
+    eo['account_name'] = account_name
+    eo['project_code'] = account_name
+    eo['queue'] = queue
+    eo['job_priority'] = job_priority
+    eo['walltime'] = walltime
+    eo['modules'] = modules
+    eo['conda_env'] = conda_env
+
+    eo['skip']= ['job_name','hpc_system','horires','parentdir','vertres', 'mres', 'input_file', 'LABEL','start_time','stop_time','secondary_start_time','secondary_stop_time','segment' ,'SOURCE_START','PRIHIST','MXHIST_PRIM','SECHIST','MXHIST_SECH','account_name','project_code','queue','job_priority','walltime']
+    
+    return engage_options
+
+def get_engage_start_time(datetime_str, seconds):
+    # Parse the input datetime string
+    dt = datetime.fromisoformat(datetime_str)
+    
+    # Subtract the seconds using timedelta
+    new_dt = dt - timedelta(seconds=seconds)
+    
+    if new_dt.time() != datetime.min.time():
+        # Adjust to the previous midnight
+        new_dt = datetime.combine(new_dt.date(), datetime.min.time())
+    
+    # Return the new datetime as a string in the same format
+    return new_dt.isoformat()
+
+def engage_run(options, debug, coupling, engage):
+    with open(OPTION_DESCRIPTIONS_FILE, "r", encoding="utf-8") as f:
+        option_descriptions = json.load(f)
+    options_standalone = copy.deepcopy(options)
+    options_coupling = copy.deepcopy(options)
+    #For standalone
+    pbs=True
+    options_standalone["simulation"]["job_name"] = f'{engage["job_name"]}-tiegcm-standalone'
+    options_standalone["inp"]["stop_time"] = engage["coupled_start_time"]
+    options_standalone["inp"]["PRIHIST"] = '1 0 0 0'
+    options_standalone["inp"]["MXHIST_PRIM"] = 1
+    options_standalone["inp"]["SECHIST"] = '0 1 0 0'
+    options_standalone["inp"]["MXHIST_SECH"] = 24
+    options_standalone["inp"]["segment"] = '1 0 0 0'
+    options_standalone["model"]["data"]["workdir"] = os.path.join(engage["parentdir"],"tiegcm_standalone")
+    if not os.path.exists(options_standalone["model"]["data"]["workdir"]):
+        os.makedirs(options_standalone["model"]["data"]["workdir"])
+    options_standalone["model"]["data"]["histdir"] = os.path.join(engage["parentdir"],"tiegcm_standalone")
+    
+    in_prim = options_standalone["inp"]["SOURCE"]
+    out_prim = f'{options_standalone["model"]["data"]["workdir"]}/{options_standalone["simulation"]["job_name"]}_prim.nc'
+    options_standalone["inp"]["SOURCE"] = out_prim
+    horires_standalone= engage["horires"]
+    vertres_standalone, mres_standalone, nres_grid_standalone, STEP_standalone = resolution_solver(horires_standalone,engage)
+    interpic (in_prim,float(horires_standalone),float(vertres_standalone),float(options_standalone['model']['specification']['zitop']),out_prim)    
+    standalone_inp_files,standalone_pbs_files, standalone_log_files,pristart_times, pristop_times=segment_inp_pbs(options_standalone, options_standalone["simulation"]["job_name"],pbs, engage)
+    #For coupled
+    pbs=False
+    options_coupling["model"]["data"]["modelexe"] = options_coupling["model"]["data"]["coupled_modelexe"]
+    coupling_modelexe=options_coupling["model"]["data"]["coupled_modelexe"]
+    horires_coupling = float(engage["horires_coupled"])
+    options_coupling["model"]["specification"]["horires"] = horires_coupling
+    vertres_coupling, mres_coupling, nres_grid_coupling, STEP_coupling = resolution_solver(horires_coupling,engage)
+    options_coupling["model"]["specification"]["vertres"] = vertres_coupling
+    options_coupling["model"]["specification"]["mres"] = mres_coupling
+    options_coupling["model"]["specification"]["nres_grid"] = nres_grid_coupling
+    options_coupling["inp"]["STEP"] = STEP_coupling
+    SOURCE_coupling = os.path.join(options_coupling["model"]["data"]["workdir"],f'{engage["job_name"]}_prim.nc')
+    options_coupling["inp"]["SOURCE"] = SOURCE_coupling #standalone_inp_files[-1]
+    options_coupling["inp"]["SOURCE_START"] = pristop_times[-1]
+    options_coupling["simulation"]["job_name"] = f'{engage["job_name"]}'
+    options_coupling["inp"]["start_time"] = engage["coupled_start_time"]
+    options_coupling["inp"]["PRIHIST"] = " ".join(str(i) for i in engage["segment"])
+    options_coupling["inp"]["MXHIST_PRIM"] = 1
+    options_coupling["inp"]["SECHIST"] = " ".join(str(i) for i in seconds_to_dhms(engage["voltron_dtOut"]))
+    options_coupling["inp"]["MXHIST_SECH"] = int(engage["segment_seconds"]/engage["voltron_dtOut"])
+    options_coupling["inp"]["GSWM_MI_DI_NCFILE"] = find_file(f'*gswm_diurn_{horires_coupling}d_99km*', TIEGCMDATA)
+    options_coupling["inp"]["GSWM_MI_SDI_NCFILE"] = find_file(f'*gswm_semi_{horires_coupling}d_99km*', TIEGCMDATA)
+    options_coupling["inp"]["GSWM_NM_DI_NCFILE"] = find_file(f'*gswm_nonmig_diurn_{horires_coupling}d_99km*', TIEGCMDATA)
+    options_coupling["inp"]["GSWM_NM_SDI_NCFILE"] = find_file(f'*gswm_nonmig_semi_{horires_coupling}d_99km*', TIEGCMDATA)
+    coupling_inp_files,coupling_pbs_files, coupling_log_files, pristart_times, pristop_times = segment_inp_pbs(options_coupling, options_coupling["simulation"]["job_name"],pbs)
+    select_coupling,ncpus_coupling,mpiprocs_coupling=select_resource_defaults(options_coupling,option_descriptions)
+    options_coupling["job"]["resource"]["select"] = select_coupling
+    options_coupling["job"]["resource"]["ncpus"] = ncpus_coupling
+    options_coupling["job"]["resource"]["mpiprocs"] = mpiprocs_coupling
+    nprocs_coupling = int(mpiprocs_coupling)*int(select_coupling)
+    options_coupling["job"]["nprocs"] = nprocs_coupling
+    with open(OPTION_DESCRIPTIONS_FILE, "r", encoding="utf-8") as f:
+        option_descriptions = json.load(f)
+    
+    return options_coupling,standalone_pbs_files,coupling_inp_files
---
+++ tiegcmrun/namelist_solver.py
@@ -0,0 +1,321 @@
+"""
+namelist_solver.py for the TIEGCMrun software.
+
+This script helps in solving for the correct input parameter for the namelist files for running the TIEGCM model. 
+
+Functions included:
+
+- inp_pri_date(start_date_str, stop_date_str): Converts start and stop date strings to datetime objects and extracts relevant information.
+- valid_hist(start_time, stop_time): Calculates valid divisions for a given time range.
+- inp_mxhist(start_time, stop_time, x_hist, mxhist_warn, segment=None): Calculates the MXHIST value based on the given start and stop times, and x_hist values.
+- inp_sechist(SECSTART, SECSTOP, segment=None): Determines the value of SECHIST based on the given SECSTART and SECSTOP.
+- inp_prihist(PRISTART, PRISTOP, segment=None): Calculates the PRIHIST list based on the PRISTART and PRISTOP values.
+- inp_pri_out(start_time, stop_time, PRIHIST, MXHIST_PRIM, pri_files, histdir, run_name): Generates the output file names and the total number of files based on the given parameters.
+- inp_sec_out(start_time, stop_time, SECHIST, MXHIST_SECH, sec_files, histdir, run_name): Calculates the output file names and the total number of files for the secondary history output.
+- inp_sec_date(start_time, stop_time, SECHIST): Calculates the SECSTART and SECSTOP values based on the given start_time, stop_time, and SECHIST.
+"""
+
+
+from datetime import datetime, timedelta
+from math import ceil
+
+
+def inp_pri_date(start_date_str, stop_date_str):
+    """
+    Convert start and stop date strings to datetime objects and extract relevant information.
+
+    Args:
+        start_date_str (str): Start date string in the format "%Y-%m-%dT%H:%M:%S".
+        stop_date_str (str): Stop date string in the format "%Y-%m-%dT%H:%M:%S".
+
+    Returns:
+        tuple: A tuple containing the following values:
+            - START_YEAR (int): The year of the start date.
+            - START_DAY (int): The day of the year of the start date.
+            - PRISTART (list): A list containing the day of the year, hour, minute, and second of the start date.
+            - PRISTOP (list): A list containing the day of the year, hour, minute, and second of the stop date.
+    """
+    # Parse the start and stop dates
+    start_time = datetime.strptime(start_date_str, "%Y-%m-%dT%H:%M:%S")
+    stop_time = datetime.strptime(stop_date_str, "%Y-%m-%dT%H:%M:%S")
+
+    # Extract START_YEAR and START_DAY
+    START_YEAR = start_time.year
+    START_DAY = start_time.timetuple().tm_yday
+
+    # Format PRISTART and PRISTOP
+    PRISTART = [start_time.timetuple().tm_yday, start_time.hour, start_time.minute, start_time.second]
+    PRISTOP = [stop_time.timetuple().tm_yday, stop_time.hour, stop_time.minute, stop_time.second]
+
+    return START_YEAR, START_DAY, PRISTART, PRISTOP
+
+def valid_hist(start_time, stop_time):
+    """
+    Calculate valid divisions for a given time range.
+
+    Parameters:
+    start_time (str): The start date in the format '%Y-%m-%dT%H:%M:%S'.
+    stop_time (str): The stop date in the format '%Y-%m-%dT%H:%M:%S'.
+
+    Returns:
+    list: A list of valid divisions for days, hours, minutes, and seconds.
+    Each division is represented as a list [days, hours, minutes, seconds].
+    """
+
+    start = datetime.strptime(start_time, '%Y-%m-%dT%H:%M:%S')
+    stop = datetime.strptime(stop_time, '%Y-%m-%dT%H:%M:%S')
+    total_duration = stop - start
+    total_seconds = total_duration.total_seconds()
+    
+    valid_divisions = []
+    
+    # Calculate valid divisions for days
+    total_days = total_duration.days
+    for n_day in range(1, total_days + 1):
+        valid_divisions.append([n_day,0,0,0])
+    
+    # Calculate valid divisions for hours
+    hours_divisions = [1, 2, 3, 4, 6, 12, 18, 24]
+    for n_hour in hours_divisions:
+        if total_seconds % (n_hour * 3600) == 0:
+            valid_divisions.append([0,n_hour,0,0])
+            
+    # Calculate valid divisions for minutes
+    minutes_divisions = [1, 2, 5, 10, 15, 30, 45, 60]
+    for n_min in minutes_divisions:
+        if total_seconds % (n_min * 60) == 0:
+            valid_divisions.append([0,0,n_min,0])
+    
+    # Calculate valid divisions for seconds
+    seconds_divisions = [1, 2, 5, 10, 15, 30, 45, 60]
+    for n_sec in seconds_divisions:
+        if total_seconds % n_sec == 0:
+            valid_divisions.append([0,0,0,n_sec])
+    
+    return valid_divisions
+
+def inp_mxhist(start_time, stop_time, x_hist, mxhist_warn, segment = None):
+    """
+    Calculate the MXHIST value based on the given start and stop times, and x_hist values.
+
+    Args:
+        start_time (str): The start time in the format '%Y-%m-%dT%H:%M:%S'.
+        stop_time (str): The stop time in the format '%Y-%m-%dT%H:%M:%S'.
+        x_hist (tuple): A tuple containing the number of days, hours, minutes, and seconds for x_hist.
+        mxhist_warn (str): A warning message for MXHIST.
+
+    Returns:
+        tuple: A tuple containing the calculated MXHIST value and the updated mxhist_warn message.
+
+    Raises:
+        None
+
+    """
+    start = datetime.strptime(start_time, '%Y-%m-%dT%H:%M:%S')
+    stop = datetime.strptime(stop_time, '%Y-%m-%dT%H:%M:%S')
+    total_duration = stop - start
+    total_seconds = total_duration.total_seconds()
+    
+    seconds_in_day = 86400
+    seconds_in_hour = 3600
+    seconds_in_min = 60
+    
+    n_day, n_hour, n_min, n_sec = x_hist
+    step_seconds = (n_day * 86400) + (n_hour * 3600) + (n_min * 60) + n_sec
+    
+    if step_seconds == 0:
+        return "Invalid prihist: step cannot be 0."
+    
+    mxhist_day = seconds_in_day / step_seconds
+    mxhist_hour = seconds_in_hour / step_seconds
+    mxhist_min = seconds_in_min / step_seconds    
+    if segment == None:
+        if mxhist_day >= 1:
+            mxhist_warn = (mxhist_warn + "\n" if mxhist_warn is not None else "") + f"For a Daily output set MXHIST to {int(mxhist_day)}"
+        if mxhist_hour >= 1:
+            mxhist_warn = mxhist_warn +  f"\nFor a Hourly output set MXHIST to {int(mxhist_hour)}"
+        if mxhist_min >= 1:
+            mxhist_warn = mxhist_warn + f"\nFor a Minutely output set MXHIST to {int(mxhist_min)}"
+        MXHIST = mxhist_day
+    else:
+        segment_seconds = (segment[0] * 86400) + (segment[1] * 3600) + (segment[2] * 60) + segment[3]
+        MXHIST = segment_seconds/step_seconds
+        mxhist_warn = f"MXHIST minimum = 1, maximum = {int(MXHIST)} for segment run."
+    return(int(MXHIST), mxhist_warn)
+
+def inp_sechist(SECSTART, SECSTOP, segment = None):
+    """
+    Determines the value of SECHIST based on the given SECSTART and SECSTOP.
+
+    Parameters:
+    SECSTART (list): A list containing the start day.
+    SECSTOP (list): A list containing the stop day.
+
+    Returns:
+    list: A list containing the value of SECHIST.
+
+    """
+    PRISTART_DAY = SECSTART[0]
+    PRISTOP_DAY = SECSTOP[0]
+    n_split_day = int(PRISTOP_DAY - PRISTART_DAY)
+    if n_split_day >= 7:
+        SECHIST = [1, 0, 0, 0]
+    else:
+        SECHIST = [0, 1, 0, 0]
+    
+    if segment != None:
+        SECHIST = [1 if x != 0 else 0 for x in segment]
+
+    return SECHIST
+
+def inp_prihist(PRISTART, PRISTOP, segment = None):
+    """
+    Calculate the PRIHIST list based on the PRISTART and PRISTOP values.
+
+    Parameters:
+    PRISTART (list): A list containing the start day of the PRIHIST period.
+    PRISTOP (list): A list containing the stop day of the PRIHIST period.
+
+    Returns:
+    list: The PRIHIST list, which is either [1, 0, 0, 0] or [0, 1, 0, 0] based on the number of days in the PRIHIST period.
+    """
+    PRISTART_DAY = PRISTART[0]
+    PRISTOP_DAY = PRISTOP[0]
+    n_split_day = int(PRISTOP_DAY - PRISTART_DAY)
+    if n_split_day >= 7:
+        PRIHIST = [1, 0, 0, 0]
+    else:
+        PRIHIST = [0, 1, 0, 0]
+
+    if segment != None:
+        PRIHIST = [1 if x != 0 else 0 for x in segment]
+    return PRIHIST
+
+def inp_pri_out(start_time, stop_time, PRIHIST, MXHIST_PRIM, pri_files, histdir, run_name):
+    """
+    Generate the output file names and the total number of files based on the given parameters.
+
+    Parameters:
+    start_time (str): The start time in the format '%Y-%m-%dT%H:%M:%S'.
+    stop_time (str): The stop time in the format '%Y-%m-%dT%H:%M:%S'.
+    PRIHIST (tuple): A tuple containing the number of days, hours, minutes, and seconds for PRIHIST.
+    MXHIST_PRIM (int): The maximum number of primary history files.
+    pri_files (int): The number of existing primary history files.
+    histdir (str): The directory where the history files are stored.
+    run_name (str): The name of the run.
+
+    Returns:
+    tuple: A tuple containing the output file names and the updated number of primary history files.
+
+    """
+    # Convert start and stop times to datetime
+    start = datetime.strptime(start_time, '%Y-%m-%dT%H:%M:%S')
+    stop = datetime.strptime(stop_time, '%Y-%m-%dT%H:%M:%S')
+    
+    # Calculate total duration in seconds
+    total_seconds = (stop - start).total_seconds()
+    
+    # Convert prihist to seconds
+    n_day, n_hour, n_min, n_sec = PRIHIST
+    step_seconds = (n_day * 86400) + (n_hour * 3600) + (n_min * 60) + n_sec
+    
+    # Calculate model data per output file in seconds
+    data_per_file_seconds = step_seconds * int(MXHIST_PRIM)
+    
+    # Calculate the total number of files, rounding up
+    number_of_files = ceil(total_seconds / data_per_file_seconds)
+    pri_files_n = pri_files + number_of_files
+    if pri_files == 0:
+        if number_of_files == 1:
+                OUTPUT = OUTPUT = f"'{histdir}/{run_name}_prim_{'{:02d}'.format(pri_files)}.nc' , '{histdir}/{run_name}_prim_{'{:02d}'.format(pri_files+1)}.nc'"
+        else:
+            PRIM_0 = f"{histdir}/{run_name}_prim_{'{:02d}'.format(pri_files)}.nc"
+            PRIM_N = f"{histdir}/{run_name}_prim_{'{:02d}'.format(pri_files_n)}.nc"
+            OUTPUT = f"'{PRIM_0}','to','{PRIM_N}','by','1'"
+    else:
+        if number_of_files == 1:
+            OUTPUT = OUTPUT = f"'{histdir}/{run_name}_prim_{'{:02d}'.format(pri_files)}.nc' , '{histdir}/{run_name}_prim_{'{:02d}'.format(pri_files+1)}.nc'"
+        else:
+            PRIM_0 = f"{histdir}/{run_name}_prim_{'{:02d}'.format(pri_files)}.nc"
+            PRIM_N = f"{histdir}/{run_name}_prim_{'{:02d}'.format(pri_files_n)}.nc"
+            OUTPUT = f"'{PRIM_0}','to','{PRIM_N}','by','1'"
+    return OUTPUT, pri_files_n
+
+def inp_sec_out(start_time, stop_time, SECHIST, MXHIST_SECH, sec_files, histdir, run_name):
+    """
+    Calculate the output file names and the total number of files for the secondary history output.
+
+    Args:
+        start_time (str): The start time of the simulation in the format '%Y-%m-%dT%H:%M:%S'.
+        stop_time (str): The stop time of the simulation in the format '%Y-%m-%dT%H:%M:%S'.
+        SECHIST (list): A list of integers representing the duration of each secondary history output file in days, hours, minutes, and seconds.
+        MXHIST_SECH (int): The maximum number of secondary history output files per primary history output file.
+        sec_files (int): The number of existing secondary history output files.
+        histdir (str): The directory where the history output files are stored.
+        run_name (str): The name of the simulation run.
+
+    Returns:
+        tuple: A tuple containing the output file name pattern and the updated number of secondary history output files.
+
+    """
+    # Convert start and stop times to datetime
+    start = datetime.strptime(start_time, '%Y-%m-%dT%H:%M:%S')
+    stop = datetime.strptime(stop_time, '%Y-%m-%dT%H:%M:%S')
+    sechist_delta = timedelta(days=SECHIST[0], hours=SECHIST[1], minutes=SECHIST[2], seconds=SECHIST[3])
+    start = start + sechist_delta
+
+    # Calculate total duration in seconds
+    total_seconds = (stop - start).total_seconds()
+
+    # Convert prihist to seconds
+    n_day, n_hour, n_min, n_sec = SECHIST
+    step_seconds = (n_day * 86400) + (n_hour * 3600) + (n_min * 60) + n_sec
+
+    # Calculate model data per output file in seconds
+    data_per_file_seconds = step_seconds * int(MXHIST_SECH)
+
+    # Calculate the total number of files, rounding up
+    number_of_files = ceil(total_seconds / data_per_file_seconds)
+    sec_files_start = sec_files + 1  # Start numbering from next file
+    sec_files_end = sec_files_start + number_of_files  # End numbering based on number of files
+
+    if number_of_files == 1:
+        # If only one file is being generated
+        SECOUT = f"'{histdir}/{run_name}_sech_{'{:02d}'.format(sec_files_start)}.nc'"
+        sec_files_end = sec_files_start
+    else:
+        # If multiple files are being generated
+        SECH_0 = f"{histdir}/{run_name}_sech_{'{:02d}'.format(sec_files_start)}.nc"
+        SECH_N = f"{histdir}/{run_name}_sech_{'{:02d}'.format(sec_files_end)}.nc"
+        SECOUT = f"'{SECH_0}','to','{SECH_N}','by','1'"
+
+    # Return the new sec_files value for subsequent calls
+    return SECOUT, sec_files_end
+
+def inp_sec_date(start_time, stop_time, SECHIST):
+    """
+    Calculate the SECSTART and SECSTOP values based on the given start_time, stop_time, and SECHIST.
+
+    Parameters:
+    start_time (str): The start time in the format '%Y-%m-%dT%H:%M:%S'.
+    stop_time (str): The stop time in the format '%Y-%m-%dT%H:%M:%S'.
+    SECHIST (list): A list containing the number of days, hours, minutes, and seconds to be added to the start time.
+
+    Returns:
+    tuple: A tuple containing the SECSTART and SECSTOP values.
+
+    Example:
+    start_time = '2022-01-01T00:00:00'
+    stop_time = '2022-01-02T00:00:00'
+    SECHIST = [1, 0, 0, 0]
+    inp_sec_date(start_time, stop_time, SECHIST)
+    # Output: ([1, 0, 0, 0], [2, 0, 0, 0])
+    """
+    start = datetime.strptime(start_time, '%Y-%m-%dT%H:%M:%S')
+    stop = datetime.strptime(stop_time, '%Y-%m-%dT%H:%M:%S')
+    sechist_delta = timedelta(days=SECHIST[0], hours=SECHIST[1], minutes=SECHIST[2], seconds=SECHIST[3])
+    start = start + sechist_delta
+    SECSTART = [start.timetuple().tm_yday,start.hour,start.minute,start.second]
+    SECSTOP = [stop.timetuple().tm_yday,stop.hour,stop.minute,stop.second]
+
+    return SECSTART, SECSTOP
