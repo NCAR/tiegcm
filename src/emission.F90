3c2ad9f9d7cb370a12c5e3927bfc6e814503db5a
Dong Lin <ldong@derecho4.hsn.de.hpc.ucar.edu>
2025-03-29 07:56:48 -0600

    First step to get VER6300

---
+++ src/emission.F90
@@ -0,0 +1,11 @@
+module emission_rates
+
+    implicit none
+    contains
+    subroutine ver6300(tn,tn_nm,un,vn,o1,mbar,xnmbar,scht,schti, &
+        cp,kt,km,hdt,qji_tn,cool_imp,cool_exp,w_upd,tn_upd,tn_nm_upd, &
+        lev0,lev1,lon0,lon1,lat0,lat1)
+
+    end subroutine dt
+
+end module emission_rates
