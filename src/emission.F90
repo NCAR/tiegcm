d3f4a17299154db30908000544690ec98a5b008e
Dong Lin <ldong@derecho6.hsn.de.hpc.ucar.edu>
2025-09-27 19:59:43 -0600

    temp 5200 emr.

--- src/emission.F90
+++ src/emission.F90
@@ -18,4 +18,17 @@ module emission_rates
 
     end subroutine ver6300
 
+    subroutine ver5200(ne,o1,te,n2, &
+        lev0,lev1,lon0,lon1,lat0,lat1)
+
+        ! volume emission rate for O(1D) at 630.0 nm
+        ! based on eq (65) of Rees and Roble (1975, Rev. of Geophys. and Space Phys.).
+        AD = 9.1e-3 ! [/s], Einstein level of O1D.
+        Le_N2D = 3.4e-18/sqrt(Te)*exp(-2.74e4/Te)*Omega*Ne*nN ! eV/cm^3/s, atomic Nitrogen loss rate
+        e2D = 2.37 ! eV, excitation potential
+        eta_2D = Le_N2D./e2D ! [/cm^3/s]
+        SD = 1.0e-12*nO2 + 8.54e-7/sqrt(Te)*Omega*Ne
+        ver = eta_2D./(1+SD./AD) ! [Photons/cm^3/s]
+
+    end subroutine ver5200
 end module emission_rates
